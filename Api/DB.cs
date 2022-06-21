fb02ff2dd8e3e02a637887ed258e59c5a7c70ef2
Alexey <alexey.e.artamonov@gmail.com>
2022-06-22 01:20:21 +0300

    Добавил приложение для "Обновления базы  данных"

--- Api/DB.cs
+++ Api/DB.cs
@@ -7,13 +7,14 @@ namespace Api
     public class DB
     {
         private static DatabaseReader reader = null;
+        private static string Path = System.IO.File.ReadAllText("Config.cfg").Remove(0, 5);
         public static Models.GeoData ReadFromDb(string ip)
         {
             if (IPAddress.TryParse(ip, out System.Net.IPAddress address) == false)
                 return null;
 
             if (reader == null)
-                reader = new DatabaseReader("ipdb.mmdb");
+                reader = new DatabaseReader(Path + "ipdb.mmdb");
 
             try
             {
---
+++ Updater/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using HtmlAgilityPack;
+using System.Xml.Linq;
+
+namespace Updater
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = File.ReadAllText("Config.cfg").Remove(0, 5);
+            HtmlWeb hw = new HtmlWeb();
+            HtmlDocument doc = hw.Load(@"https://db-ip.com/db/download/ip-to-city-lite");
+
+            string downloadLink = null;
+
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                string temp = link.GetAttributeValue("href", "-");
+                if (temp.EndsWith(".mmdb.gz"))
+                {
+                    downloadLink = temp;
+                    break;
+                }
+            }
+
+            if (downloadLink == null)
+                return;
+
+            WebClient Client = new WebClient();
+            Client.DownloadFile(downloadLink, path + "mmdb.gz");
+
+            using (FileStream fInStream = new FileStream(path + "mmdb.gz", FileMode.Open, FileAccess.Read))
+            {
+                using (GZipStream zipStream = new GZipStream(fInStream, CompressionMode.Decompress))
+                {
+                    using (FileStream fOutStream = new FileStream(path + "ipdb.mmdb", FileMode.Create, FileAccess.Write))
+                    {
+                        byte[] tempBytes = new byte[4096];
+                        int i;
+                        while ((i = zipStream.Read(tempBytes, 0, tempBytes.Length)) != 0)
+                        {
+                            fOutStream.Write(tempBytes, 0, i);
+                        }
+                    }
+                }
+            }
+
+            File.Delete(path + "mmdb.gz");
+        }
+    }
+}
